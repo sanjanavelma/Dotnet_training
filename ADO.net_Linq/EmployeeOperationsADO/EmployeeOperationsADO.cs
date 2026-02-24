9c46b0383c2bf76db2a93e59f10d9e3daa0cbff3
Sanjana_velma <sanjanavelma27@gmail.com>
2026-02-24 10:40:53 +0530

    Committing

---
+++ ADO.net_Linq/EmployeeOperationsADO/EmployeeOperationsADO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.net_Linq.EmployeeOperationsADO
+{
+    public class EmployeeOperationsADO
+    {
+        
+    }
+}
---
+++ ADO.net_Linq/EmployeeOperationsADO/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+class Program
+{
+    static string connectionString =
+        "Server=localhost\\SQLEXPRESS;Database=EmployeeOperationsDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    static void Main()
+    {
+        Console.Write("Enter Department: ");
+        string dept = Console.ReadLine();
+
+        GetEmployeesByDepartment(dept);
+        GetDepartmentCount(dept);
+        GetEmployeeOrders();
+        GetDuplicateEmployees();
+    }
+
+    // Part 1
+    static void GetEmployeesByDepartment(string dept)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("sp_GetEmployeesByDepartment", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Department", dept);
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Console.WriteLine("\nEmployees in Department:");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["EmpId"]} - {reader["Name"]} - {reader["Phone"]}");
+            }
+
+            reader.Close();
+        }
+    }
+
+    // Part 2
+    static void GetDepartmentCount(string dept)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("sp_GetDepartmentEmployeeCount", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Department", dept);
+
+            SqlParameter outputParam = new SqlParameter("@TotalEmployees", SqlDbType.Int);
+            outputParam.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(outputParam);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine($"\nTotal employees in {dept}: {outputParam.Value}");
+        }
+    }
+
+    // Part 3
+    static void GetEmployeeOrders()
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("sp_GetEmployeeOrders", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Console.WriteLine("\nEmployee Orders:");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Name"]} - {reader["OrderId"]} - {reader["OrderAmount"]}");
+            }
+
+            reader.Close();
+        }
+    }
+
+    // Part 4
+    static void GetDuplicateEmployees()
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("sp_GetDuplicateEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Console.WriteLine("\nDuplicate Employees:");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["EmpId"]} - {reader["Name"]} - {reader["Phone"]}");
+            }
+
+            reader.Close();
+        }
+    }
+}
